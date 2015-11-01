fa40316e376e0a171b34192e43d0ba40eeac2839
Claire Yuan <claireyuan@Claires-MacBook-Pro.local>
2015-11-01 01:26:49 -0400

    rideapp

--- rideadmin/rideapp/models.py
+++ rideadmin/rideapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from country.py import CountryField
 # Create your models here.
 
 class RideUser(User):
@@ -9,12 +10,20 @@ class RideUser(User):
         ('M','Male'),
         )
     gender = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
-    age = 
+    age = models.IntegerField()
+    wechat =models.CharField()
+    phone = models.CharField()
+    residence = model.CharField()
+    citizenship = CountryField()
+    department_at_duke= models.CharField()
+    undergraduate_school = models.CharField()
+    company= models.CharField()
     
-    
-
 class Volunteer(RideUser):
-
+    flight
+    luggage_checked_num=
+    luggage_carryon-num=
+    backpack_num=
 
 class NewStudent(RideUser):
 
